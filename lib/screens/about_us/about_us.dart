77025da19e82dbaf902b79d9dfb1d77bb648256f
isacco <isacco.borsani@h2tcoin.com>
2023-04-17 16:48:29 +0200

    fix

--- lib/screens/about_us/about_us.dart
+++ lib/screens/about_us/about_us.dart
@@ -3,6 +3,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutUs extends StatelessWidget {
   const AboutUs({super.key});
@@ -18,13 +19,32 @@ class AboutUs extends StatelessWidget {
 
           children: [
             Container(
-              padding: const EdgeInsets.fromLTRB(50, 100, 50, 30),
-              child: Center(child: Image.asset('images/logo.png', fit: BoxFit.fill, scale: 0.5)),
+              constraints: const BoxConstraints(maxWidth: 700),
+              padding: const EdgeInsets.fromLTRB(20, 80, 20, 50),
+              child: Center(
+                  child: Image.asset('images/logo.png',
+                      fit: BoxFit.fill, scale: 0.5)),
             ),
             Container(
-              child: const Center(child: Text('ciao', style: TextStyle(color: Colors.black)))
-            )
+                child: Center(
+                    child: Column(
+              children: [
+                const Text('AiliA SA', style: TextStyle(color: Colors.black)),
+                const Text('Hertistrasse 25a 6300 Zug, CH',
+                    style: TextStyle(color: Colors.black)),
+                const Text(
+                    'For more details on company registration you can visit the following link to the Zug Canton Commercial Register, by clicking the button underneath.',
+                    style: TextStyle(color: Colors.black)),
+                CupertinoButton(child: Text('ciao'), onPressed: _launchUrl(Uri.parse('https://flutter.dev')))
+              ],
+            )))
           ],
         ));
+  }
+
+  Future<void> _launchUrl() async {
+    if (!await launchUrl(_url)) {
+      throw Exception('Could not launch $_url');
+    }
   }
 }
