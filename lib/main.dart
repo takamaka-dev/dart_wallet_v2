c33855b0ae283b25aee54c5b3599bbfcaa697585
isacco <isacco.borsani@h2tcoin.com>
2023-05-04 18:08:29 +0200

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+
+
+import 'package:flutter/rendering.dart';
+
 import 'app.dart';
 import '/screens/splash/splash_state.dart';
 
 void main() {
+  debugPaintSizeEnabled = false;
   runAppWithOptions(
       envFileName: '.env',
       splashState: SplashState(),
--- lib/screens/transactions/pay/pay.dart
+++ lib/screens/transactions/pay/pay.dart
@@ -6,13 +6,11 @@ class Pay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-        child: Container(
-      padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Row(
-            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               CupertinoButton(
                 onPressed: () {
@@ -20,17 +18,84 @@ class Pay extends StatelessWidget {
                       context); // Navigate back when back button is pressed
                 },
                 child: const Icon(Icons.arrow_back),
-              )
+              ),
+              Container(
+                  width: 100,
+                  height: 60,
+                  decoration: const BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius:
+                          BorderRadius.only(bottomLeft: Radius.circular(10))),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(CupertinoIcons.paperplane,
+                          size: 20, color: Colors.white),
+                      SizedBox(width: 10),
+                      Text("Pay", style: TextStyle(color: Colors.white))
+                    ],
+                  ))
             ],
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [Text("From"), SizedBox(width: 1000), Text("To")],
+          Container(
+            padding: const EdgeInsets.fromLTRB(50, 50, 50, 50),
+            child: Column(
+              children: [
+                const CupertinoTextField(
+                  textAlign: TextAlign.center,
+                  placeholder: "Address",
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        // Codice da eseguire al clic del bottone
+                      },
+                      child: const CircleAvatar(
+                        radius: 30.0,
+                        backgroundColor: Colors.green,
+                        child: Text("TKG", style: TextStyle(color: Colors.white))
+                      ),
+                    ),
+                    const SizedBox(width: 30),
+                    TextButton(
+                      onPressed: () {
+                        // Codice da eseguire al clic del bottone
+                      },
+                      child: const CircleAvatar(
+                        radius: 30.0,
+                        backgroundColor: Colors.grey,
+                        child: Text("TKR", style: TextStyle(color: Colors.black54))
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 20),
+                const CupertinoTextField(
+                  textAlign: TextAlign.center,
+                  placeholder: "Amount (EUR)",
+                ),
+                SizedBox(height: 20),
+                const CupertinoTextField(
+                  textAlign: TextAlign.center,
+                  placeholder: "TKG",
+                ),
+                SizedBox(height: 20),
+                const CupertinoTextField(
+                  decoration: BoxDecoration(
+
+                    color: CupertinoColors.white,
+                  ),
+                  placeholder: 'Inserisci qui il testo',
+                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                )
+              ],
+            ),
           )
         ],
       ),
-    ).asGlass(
-            tintColor: Colors.transparent,
-            clipBorderRadius: BorderRadius.circular(15.0)));
+    );
   }
 }
