8cde8dd5986343d57c19c9eaea08befa6fa1e5ee
isacco <isacco.borsani@h2tcoin.com>
2023-05-05 11:12:44 +0200

    fix

--- lib/screens/transactions/pay/pay.dart
+++ lib/screens/transactions/pay/pay.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:glass/glass.dart';
 
-class Pay extends StatelessWidget {
+class Pay extends StatefulWidget {
+  const Pay({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _PayState();
+}
+
+class _PayState extends State<Pay> {
+  String? currentToken;
+
+  Future<bool> _initPayInterface() async {
+    setState(() {
+      currentToken = "TKG";
+    });
+
+    return true;
+  }
+
+  @override
+  void initState() {
+    _initPayInterface();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
@@ -45,57 +67,121 @@ class Pay extends StatelessWidget {
                   textAlign: TextAlign.center,
                   placeholder: "Address",
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     TextButton(
                       onPressed: () {
-                        // Codice da eseguire al clic del bottone
+                        setState(() {
+                          currentToken = "TKG";
+                          print(currentToken);
+                        });
                       },
-                      child: const CircleAvatar(
-                        radius: 30.0,
-                        backgroundColor: Colors.green,
-                        child: Text("TKG", style: TextStyle(color: Colors.white))
-                      ),
+                      child: CircleAvatar(
+                          radius: 30.0,
+                          backgroundColor: currentToken == "TKG"
+                              ? Colors.green
+                              : Colors.grey,
+                          child: const Text("TKG",
+                              style: TextStyle(color: Colors.white))),
                     ),
                     const SizedBox(width: 30),
                     TextButton(
                       onPressed: () {
-                        // Codice da eseguire al clic del bottone
+                        setState(() {
+                          currentToken = "TKR";
+                          print(currentToken);
+                        });
                       },
-                      child: const CircleAvatar(
-                        radius: 30.0,
-                        backgroundColor: Colors.grey,
-                        child: Text("TKR", style: TextStyle(color: Colors.black54))
-                      ),
+                      child: CircleAvatar(
+                          radius: 30.0,
+                          backgroundColor:
+                              currentToken == "TKR" ? Colors.red : Colors.grey,
+                          child: Text("TKR",
+                              style: TextStyle(color: currentToken == "TKR" ? Colors.white : Colors.black54))),
                     )
                   ],
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 const CupertinoTextField(
                   textAlign: TextAlign.center,
                   placeholder: "Amount (EUR)",
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 const CupertinoTextField(
                   textAlign: TextAlign.center,
                   placeholder: "TKG",
                 ),
-                SizedBox(height: 20),
-                const CupertinoTextField(
-                  decoration: BoxDecoration(
+                const SizedBox(height: 20),
+                InputTextWithAnimation()
+                /*CupertinoTextField(
+                  maxLines: 10,
+                  placeholder: 'Enter your text here',
 
+                ),*/
+                /*const CupertinoTextField(
+                  decoration: BoxDecoration(
                     color: CupertinoColors.white,
                   ),
                   placeholder: 'Inserisci qui il testo',
-                  padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                )
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                )*/
               ],
             ),
           )
         ],
       ),
+    );
+  }
+}
+
+class InputTextWithAnimation extends StatefulWidget {
+  @override
+  _InputTextWithAnimationState createState() => _InputTextWithAnimationState();
+}
+
+class _InputTextWithAnimationState extends State<InputTextWithAnimation> {
+  bool _isFocused = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnimatedContainer(
+          duration: Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+          width: 300.0,
+          height: 60.0,
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: _isFocused ? Colors.blue : Colors.grey,
+                width: 2.0,
+              ),
+            ),
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: 'Enter your text here',
+              border: InputBorder.none,
+            ),
+            onChanged: (value) {},
+            onTap: () {
+              setState(() {
+                _isFocused = true;
+              });
+            },
+            onSubmitted: (value) {
+              setState(() {
+                _isFocused = false;
+              });
+            },
+          ),
+        ),
+      ],
     );
   }
 }
