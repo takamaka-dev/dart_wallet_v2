f4d5b18ea56acdf0e6b43ca78bd11b1dcaba5ab9
isacco <isacco.borsani@h2tcoin.com>
2023-05-19 16:26:31 +0200

    fix

--- lib/screens/transactions/transaction_list/single_transaction.dart
+++ lib/screens/transactions/transaction_list/single_transaction.dart
@@ -1,4 +1,7 @@
+import 'package:dart_wallet_v2/config/styles.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:io_takamaka_core_wallet/io_takamaka_core_wallet.dart';
 
 class SingleTransaction extends StatelessWidget {
@@ -8,18 +11,85 @@ class SingleTransaction extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     IconData i = CupertinoIcons.paperplane;
 
     if (tm.transactionType == "STAKE") {
+      i = CupertinoIcons.hammer;
+    } else if (tm.transactionType == "STAKE_UNDO") {
       i = CupertinoIcons.hand_raised_fill;
+    } else if (tm.transactionType == "BLOB") {
+      i = CupertinoIcons.doc_on_clipboard_fill;
     }
 
-    return Row(
-      children: [
+    DateTime date = DateTime.fromMillisecondsSinceEpoch(tm.notBefore);
+    String formattedDateTime = DateFormat("dd/MM/yyyy HH:mm").format(date);
 
-      ],
-    );
+    return Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          color: Colors.transparent,
+        ),
+        child: Column(
+          children: [
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Flexible(
+                    child: Container(
+                  padding: const EdgeInsets.all(12),
+                  alignment: Alignment.topLeft,
+                  decoration: BoxDecoration(
+                      color: Colors.grey.shade300,
+                      border: const Border(
+                        right: BorderSide(width: 1.0, color: Colors.white),
+                      )),
+                  child: CupertinoButton(
+                      color: Colors.grey.shade300,
+                      alignment: Alignment.topLeft,
+                      padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
+                      borderRadius: BorderRadius.zero,
+                      // Rimuove il bordo arrotondato
+
+                      onPressed: () => {},
+                      child: Column(children: [
+                        Row(
+                          children: [
+                            Icon(i, color: Styles.takamakaColor),
+                            const SizedBox(width: 10),
+                            Text(
+                              tm.transactionType.replaceAll("_", " "),
+                              style: TextStyle(
+                                  color: Styles.takamakaColor, fontSize: 17),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        Container(
+                          height: 1.0,
+                          color: Styles.takamakaColor.withOpacity(0.25),
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(formattedDateTime,
+                                style: TextStyle(color: Styles.takamakaColor)),
+                            Text(
+                                "TKG ${tm.greenValue == null ? BigInt.from(0) : tm.greenValue! / BigInt.from(10).pow(9)}",
+                                style: TextStyle(color: Styles.takamakaColor)),
+                            const SizedBox(width: 10),
+                            Text(
+                                "TKR ${tm.redValue == null ? BigInt.from(0) : tm.redValue! / BigInt.from(10).pow(9)}",
+                                style: TextStyle(color: Styles.takamakaColor))
+                          ],
+                        )
+                      ])),
+                ))
+              ],
+            ),
+            const SizedBox(height: 20)
+          ],
+        ));
   }
-
 }
--- lib/screens/transactions/transaction_list/transaction_list.dart
+++ lib/screens/transactions/transaction_list/transaction_list.dart
@@ -4,47 +4,75 @@ import 'package:dart_wallet_v2/screens/transactions/transaction_list/single_tran
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:io_takamaka_core_wallet/io_takamaka_core_wallet.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 
 class TransactionList extends StatefulWidget {
   const TransactionList({super.key});
 
   @override
   State<StatefulWidget> createState() => _TransactionListState();
-
 }
 
 class _TransactionListState extends State<TransactionList> {
 
+
   List<Widget> transactionList = [];
 
-
-  Future<bool> _initTransactionListInterface() async {
-
-    TransactionSearchInput tsi = TransactionSearchInput(50, "from", true, Globals.instance.selectedFromAddress);
+  Future<bool> _initTransactionListInterface(int size) async {
+    List<Widget> trxListTemp = [];
+    context.loaderOverlay.show();
+    TransactionSearchInput tsi = TransactionSearchInput(
+        size, "from", true, Globals.instance.selectedFromAddress);
 
     final response = await ConsumerHelper.doRequest(
-        HttpMethods.POST, ApiList().apiMap[Globals.instance.selectedNetwork]!["listransactions"]!, tsi.toJson());
+        HttpMethods.POST,
+        ApiList().apiMap[Globals.instance.selectedNetwork]!["listransactions"]!,
+        tsi.toJson());
 
     TransactionResultList trl = TransactionResultList.fromJsonArray(response);
 
     trl.transactions.forEach((element) {
-      transactionList.add(SingleTransaction(element));
+      trxListTemp.add(SingleTransaction(element));
     });
 
     setState(() {
-      //transactionList = trl.transactions;
+      transactionList = trxListTemp;
     });
+
+    context.loaderOverlay.hide();
 
     return true;
   }
 
   @override
   void initState() {
-    _initTransactionListInterface();
+    _initTransactionListInterface(50);
     //fetchMyObjects();
     super.initState();
   }
-  
+
+  List<String> filters = ["50", "100", "150", "200"];
+
+  void _showDialog(Widget child) {
+    showCupertinoModalPopup<void>(
+        context: context,
+        builder: (BuildContext context) => Container(
+              height: 216,
+              padding: const EdgeInsets.only(top: 6.0),
+              // The Bottom margin is provided to align the popup above the system navigation bar.
+              margin: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
+              // Provide a background color for the popup.
+              color: CupertinoColors.systemBackground.resolveFrom(context),
+              // Use a SafeArea widget to avoid system overlaps.
+              child: SafeArea(
+                top: false,
+                child: child,
+              ),
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -65,7 +93,7 @@ class _TransactionListState extends State<TransactionList> {
                               context); // Navigate back when back button is pressed
                         },
                         child:
-                        const Icon(Icons.arrow_back, color: Colors.white),
+                            const Icon(Icons.arrow_back, color: Colors.white),
                       ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -77,6 +105,44 @@ class _TransactionListState extends State<TransactionList> {
                       ),
                     ],
                   )),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text("Items number: ${transactionList.length}"),
+                const SizedBox(width: 10),
+                CupertinoButton(
+                    color: Colors.grey.shade200,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(CupertinoIcons.search, color: Styles.takamakaColor.withOpacity(0.6)),
+                        const SizedBox(width: 10),
+                        Text("Window result size",
+                            style: TextStyle(
+                                color: Styles.takamakaColor.withOpacity(0.6)))
+                      ],
+                    ),
+                    onPressed: () {
+                      _showDialog(CupertinoPicker(
+                        magnification: 1.22,
+                        squeeze: 1.2,
+                        useMagnifier: true,
+                        itemExtent: 32.0,
+                        onSelectedItemChanged: (int selectedItem) {
+                          _initTransactionListInterface(int.parse(filters[selectedItem]));
+                        },
+                        children:
+                            List<Widget>.generate(filters.length, (int index) {
+                          return Center(
+                            child: Text(
+                              filters[index],
+                            ),
+                          );
+                        }),
+                      ));
+                    })
+              ],
             ),
             Container(
               padding: const EdgeInsets.fromLTRB(50, 50, 50, 50),
