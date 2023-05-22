00ff949490c6dc0180def454769f74687ef2efa3
isacco <isacco.borsani@h2tcoin.com>
2023-05-22 17:27:47 +0200

    fix

--- lib/screens/tag_list/tagList.dart
+++ lib/screens/tag_list/tagList.dart
@@ -3,18 +3,29 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TagList extends StatefulWidget {
-  TagList(this.tags, {super.key});
+  TagList(this.tags, this.tagAlignment, this.tagsBrickColor, this.deleteButtonColor, {super.key});
 
+  MainAxisAlignment tagAlignment;
+  Color tagsBrickColor;
+  Color deleteButtonColor;
   List<String> tags = [];
 
   @override
-  State<StatefulWidget> createState() => _TagListState(tags);
+  State<StatefulWidget> createState() => _TagListState(
+      tags,
+      tagAlignment,
+      tagsBrickColor,
+      deleteButtonColor
+  );
 }
 
 class _TagListState extends State<TagList> {
 
-  _TagListState(this.tags);
+  _TagListState(this.tags, this.tagAlignment, this.tagsBrickColor, this.deleteButtonColor);
 
+  Color tagsBrickColor;
+  Color deleteButtonColor;
+  MainAxisAlignment tagAlignment;
   List<String> tags = [];
 
   @override
@@ -49,18 +60,18 @@ class _TagListState extends State<TagList> {
       //width: cellwidth
 
       child: CupertinoButton(
-          color: Styles.takamakaColor.withOpacity(0.9),
+          color: tagsBrickColor,
           alignment: Alignment.topLeft,
           padding: const EdgeInsets.fromLTRB(16.0, 0.0, 0.0, 0.0),
           borderRadius: BorderRadius.zero,
           onPressed: () => {},
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: tagAlignment,
             children: [
               Expanded(child: Text(tag)),
               RawMaterialButton(
                   elevation: 2.0,
-                  fillColor: Colors.red.shade300,
+                  fillColor: deleteButtonColor,
                   shape: const RoundedRectangleBorder(
                       borderRadius: BorderRadius.zero
                   ),
