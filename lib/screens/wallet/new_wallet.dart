c5a5c9b68bbc51368736233a85a558552644d802
isacco <isacco.borsani@h2tcoin.com>
2023-05-24 15:46:13 +0200

    fix

--- lib/screens/wallet/new_wallet.dart
+++ lib/screens/wallet/new_wallet.dart
@@ -7,91 +7,143 @@ import 'package:glass/glass.dart';
 import 'package:io_takamaka_core_wallet/io_takamaka_core_wallet.dart';
 import 'package:loader_overlay/loader_overlay.dart';
 
-class NewWallet extends StatelessWidget {
-  NewWallet({super.key, required this.onRefresh});
+class NewWallet extends StatefulWidget {
+  const NewWallet({super.key, required this.onRefresh});
+
+  final VoidCallback onRefresh;
+
+  @override
+  State<StatefulWidget> createState() => _NewWalletState(onRefresh);
+}
+
+class _NewWalletState extends State<NewWallet> {
+  _NewWalletState(this.onRefresh);
+
+  TextEditingController controllerPassword = TextEditingController();
+  TextEditingController controllerRepassword = TextEditingController();
 
   final VoidCallback onRefresh;
   String password = "";
   String walletName = "";
 
+  bool errorPassword = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                CupertinoButton(
-                  onPressed: () {
-                    Navigator.pop(
-                        context); // Navigate back when back button is pressed
-                  },
-                  child: const Icon(Icons.arrow_back),
-                )
-              ],
-            ),
-            Container(
-              constraints: const BoxConstraints(maxWidth: 700),
-              padding: const EdgeInsets.fromLTRB(20, 80, 20, 50),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Center(
-                      child: Image.asset('images/logo_small.png', width: 100)),
-                  const SizedBox(height: 20),
-                  Text(
-                      style:
-                          TextStyle(fontSize: 25, color: Colors.grey.shade600),
-                      "Add a new wallet"),
-                  const SizedBox(height: 50),
-                  CupertinoTextField(
-                    textAlign: TextAlign.center,
-                    placeholder: "Wallet name",
-                    onChanged: (value) => {walletName = value},
-                  ),
-                  const SizedBox(height: 50),
-                  CupertinoTextField(
-                    obscureText: true,
-                    textAlign: TextAlign.center,
-                    placeholder: "Password",
-                    onChanged: (value) => {password = value},
-                  ),
-                  const SizedBox(height: 50),
-                  CupertinoButton(
-                      color: Styles.takamakaColor,
-                      onPressed: () => {_openWallet(context)},
-                      child: const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(CupertinoIcons.plus),
-                          Text(' Create wallet'),
-                        ],
-                      )
-                  )
-                ],
+    return SingleChildScrollView(
+      child: CupertinoPageScaffold(
+        child: Container(
+          child: Column(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: Container(
+                    color: Styles.takamakaColor,
+                    child: Stack(
+                      alignment: Alignment.centerLeft,
+                      children: <Widget>[
+                        CupertinoButton(
+                          onPressed: () {
+                            Navigator.pop(
+                                context); // Navigate back when back button is pressed
+                          },
+                          child:
+                              const Icon(Icons.arrow_back, color: Colors.white),
+                        ),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text("New wallet (step 1/3)",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white)),
+                          ],
+                        ),
+                      ],
+                    )),
               ),
-            ).asGlass(
-                tintColor: Colors.transparent,
-                clipBorderRadius: BorderRadius.circular(15.0))
-          ],
+              Container(
+                padding: const EdgeInsets.fromLTRB(50, 50, 50, 50),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Center(
+                        child: Icon(
+                      Icons.wallet,
+                      color: Styles.takamakaColor.withOpacity(0.9),
+                      size: 80,
+                    )),
+                    const SizedBox(height: 20),
+                    Text(
+                        style: TextStyle(
+                            fontSize: 25, color: Colors.grey.shade600),
+                        "Add a new wallet"),
+                    const SizedBox(height: 50),
+                    CupertinoTextField(
+                      textAlign: TextAlign.center,
+                      placeholder: "Wallet name",
+                      onChanged: (value) => {walletName = value},
+                    ),
+                    const SizedBox(height: 30),
+                    errorPassword
+                        ? Column(children: [
+                            Text("The password fields do not match",
+                                style: TextStyle(
+                                    color: Colors.red.withOpacity(0.8))),
+                            const SizedBox(height: 20)
+                          ])
+                        : const Text(""),
+                    CupertinoTextField(
+                      obscureText: true,
+                      textAlign: TextAlign.center,
+                      placeholder: "Password",
+                      controller: controllerPassword,
+                      onChanged: (value) => {password = value},
+                    ),
+                    const SizedBox(height: 50),
+                    CupertinoTextField(
+                      obscureText: true,
+                      textAlign: TextAlign.center,
+                      controller: controllerRepassword,
+                      placeholder: "Retype password",
+                      onChanged: (value) => {password = value},
+                    ),
+                    const SizedBox(height: 50),
+                    CupertinoButton(
+                        color: Styles.takamakaColor,
+                        onPressed: () => {_openWallet(context)},
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(CupertinoIcons.plus),
+                            Text(' Create wallet'),
+                          ],
+                        ))
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
   _openWallet(BuildContext context) async {
-    context.loaderOverlay.show();
-    /*await WalletUtils.initWallet(
+    if (controllerPassword.text != controllerRepassword.text) {
+      setState(() {
+        errorPassword = true;
+      });
+    } else {
+      context.loaderOverlay.show();
+      /*await WalletUtils.initWallet(
         'wallets', walletName, dotenv.get('WALLET_EXTENSION'), password);*/
 
-    Globals.instance.generatedWordsPreInitWallet = await WordsUtils.generateWords();
+      Globals.instance.generatedWordsPreInitWallet =
+          await WordsUtils.generateWords();
 
-
-
-    onRefresh();
-    context.loaderOverlay.hide();
-    Navigator.pop(context,true);
+      onRefresh();
+      context.loaderOverlay.hide();
+      Navigator.pop(context, true);
+    }
   }
 }
