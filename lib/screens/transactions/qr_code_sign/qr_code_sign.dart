3f70f7703d222c22dcad07856d7422394632b2d9
isacco <isacco.borsani@h2tcoin.com>
2023-06-21 14:59:24 +0200

    fix

--- lib/screens/transactions/qr_code_sign/qr_code_sign.dart
+++ lib/screens/transactions/qr_code_sign/qr_code_sign.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
 
 import 'package:cryptography/cryptography.dart';
+import 'package:cryptography/cryptography.dart';
+import 'package:cryptography/cryptography.dart';
+import 'package:cryptography/dart.dart';
 import 'package:dart_wallet_v2/config/globals.dart';
 import 'package:dart_wallet_v2/config/styles.dart';
 import 'package:flutter/cupertino.dart';
@@ -29,40 +32,40 @@ class _QrCodeSignState extends State<QrCodeSign> {
             const Text('testScan'),
           ],
         )),
-    // QRCodeDartScanView(
-    //   scanInvertedQRCode: true, // enable scan invert qr code ( default = false)
-    //   typeScan: TypeScan.live, // if TypeScan.takePicture will try decode when click to take a picture (default TypeScan.live)
-    //   // takePictureButtonBuilder: (context,controller,isLoading){ // if typeScan == TypeScan.takePicture you can customize the button.
-    //   //    if(loading) return CircularProgressIndicator();
-    //   //    return ElevatedButton(
-    //   //       onPressed:controller.takePictureAndDecode,
-    //   //       child:Text('Take a picture'),
-    //   //    );
-    //   // }
-    //   // resolutionPreset: = QrCodeDartScanResolutionPreset.high,
-    //   // formats: [ // You can restrict specific formats.
-    //   //   BarcodeFormat.QR_CODE,
-    //   //   BarcodeFormat.AZTEC,
-    //   //   BarcodeFormat.DATA_MATRIX,
-    //   //   BarcodeFormat.PDF_417,
-    //   //   BarcodeFormat.CODE_39,
-    //   //   BarcodeFormat.CODE_93,
-    //   //   BarcodeFormat.CODE_128,
-    //   //  BarcodeFormat.EAN_8,
-    //   //   BarcodeFormat.EAN_13,
-    //   // ],
-    //   onCapture: (Result result) {
-    //     verifyTransactionAndCallback(result);
-    //     // do anything with result
-    //     // result.text
-    //     // result.rawBytes
-    //     // result.resultPoints
-    //     // result.format
-    //     // result.numBits
-    //     // result.resultMetadata
-    //     // result.time
-    //   },
-    // )
+      // QRCodeDartScanView(
+      //   scanInvertedQRCode: true, // enable scan invert qr code ( default = false)
+      //   typeScan: TypeScan.live, // if TypeScan.takePicture will try decode when click to take a picture (default TypeScan.live)
+      //   // takePictureButtonBuilder: (context,controller,isLoading){ // if typeScan == TypeScan.takePicture you can customize the button.
+      //   //    if(loading) return CircularProgressIndicator();
+      //   //    return ElevatedButton(
+      //   //       onPressed:controller.takePictureAndDecode,
+      //   //       child:Text('Take a picture'),
+      //   //    );
+      //   // }
+      //   // resolutionPreset: = QrCodeDartScanResolutionPreset.high,
+      //   // formats: [ // You can restrict specific formats.
+      //   //   BarcodeFormat.QR_CODE,
+      //   //   BarcodeFormat.AZTEC,
+      //   //   BarcodeFormat.DATA_MATRIX,
+      //   //   BarcodeFormat.PDF_417,
+      //   //   BarcodeFormat.CODE_39,
+      //   //   BarcodeFormat.CODE_93,
+      //   //   BarcodeFormat.CODE_128,
+      //   //  BarcodeFormat.EAN_8,
+      //   //   BarcodeFormat.EAN_13,
+      //   // ],
+      //   onCapture: (Result result) {
+      //     verifyTransactionAndCallback(result);
+      //     // do anything with result
+      //     // result.text
+      //     // result.rawBytes
+      //     // result.resultPoints
+      //     // result.format
+      //     // result.numBits
+      //     // result.resultMetadata
+      //     // result.time
+      //   },
+      // )
     );
   }
 
@@ -79,6 +82,14 @@ class _QrCodeSignState extends State<QrCodeSign> {
     tb.signature = "pY2KAdaB6LgOIP91tYw7rHEFSb87tVboXhKBtndmZPy03HEsL_YvEzHrN1a7BEr1g1nfi9FEdPUiJGIHSF1mBw..";
     tb.walletCypher = "Ed25519BC";
 
+    var keyRd = SimpleKeyPairData(
+        [],
+        publicKey: SimplePublicKey(base64.decode(StringUtilities.convertFromBase64UrlSafeToBase64(tb.publicKey!)),
+            type: KeyPairType.ed25519), type: KeyPairType.ed25519);
+
+
+
+
     // TransactionBean tb = TransactionBean();
     // tb.message ="{\"from\":\"5Ra-N-s9zJqgoAzrKxWZ3O9oluhP_WYL3K46IRJU2X4.\",\"to\":\"eTMddu48tLU2uNmB_husTiRzmDLFG3e63n2aTi7N4Vg.\",\"message\":\"pullus\",\"notBefore\":1687271102595,\"redValue\":500,\"greenValue\":null,\"transactionType\":\"PAY\",\"transactionHash\":\"TpjZSrvV32pGscCmse1zhoPWPSyjbJbZKjDHSEjKIE8.\",\"epoch\":null,\"slot\":null}";
     // tb.publicKey = "5Ra-N-s9zJqgoAzrKxWZ3O9oluhP_WYL3K46IRJU2X4.";
@@ -91,6 +102,16 @@ class _QrCodeSignState extends State<QrCodeSign> {
     SimplePublicKey s = SimplePublicKey(base64.decode(base64key), type: KeyPairType.ed25519);
     SimpleKeyPair kp = SimpleKeyPairData([], publicKey: s, type: KeyPairType.ed25519);
 
+
+    Signature sigExt = Signature(StringUtilities.fromB64UrlToBytes(tb.signature!), publicKey: await kp.extractPublicKey());
+
+    final algorithm = DartEd25519(sha512: Sha512());
+    var message = (tb.message! + tb.randomSeed! + tb.walletCypher!);
+    final isSignatureCorrect = await algorithm.verifyString(
+      message,
+      signature: sigExt,
+    );
+
     String tbJson = jsonEncode(tb);
     await CryptoMisc.veryfySign(kp, tbJson, tb.signature!);
     TransactionBox verifiedTB =
@@ -98,20 +119,20 @@ class _QrCodeSignState extends State<QrCodeSign> {
     bool isVerified = verifiedTB.valid??false;
 
     if(isVerified){
-       InternalTransactionBean itb = BuilderItb.blob(
-           Globals.instance.selectedFromAddress,
-           tbJson,
-           TKmTK.getTransactionTime());
-       TransactionBean gtb = await TkmWallet.createGenericTransaction(
-           itb, skp, Globals.instance.selectedFromAddress);
-       String tb2Json = jsonEncode(gtb);
-       TransactionBox signedTbox =
-       await TkmWallet.verifyTransactionIntegrity(tb2Json, skp);
-       Map<String, dynamic> stb = signedTbox.tb!.toJson();
-       final response = await ConsumerHelper.doRequest(
-           HttpMethods.POST,
-           ApiList().apiMap['local']!["txverifywebsite"]!,
-           stb);
+      InternalTransactionBean itb = BuilderItb.blob(
+          Globals.instance.selectedFromAddress,
+          tbJson,
+          TKmTK.getTransactionTime());
+      TransactionBean gtb = await TkmWallet.createGenericTransaction(
+          itb, skp, Globals.instance.selectedFromAddress);
+      String tb2Json = jsonEncode(gtb);
+      TransactionBox signedTbox =
+      await TkmWallet.verifyTransactionIntegrity(tb2Json, skp);
+      Map<String, dynamic> stb = signedTbox.tb!.toJson();
+      final response = await ConsumerHelper.doRequest(
+          HttpMethods.POST,
+          ApiList().apiMap['local']!["txverifywebsite"]!,
+          stb);
       // inviarla in post a takamaka
     }
 
