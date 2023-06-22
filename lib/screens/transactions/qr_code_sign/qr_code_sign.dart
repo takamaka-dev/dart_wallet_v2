6ac054fd2fbff18d14e2e9149321fb258447a37b
isacco <isacco.borsani@h2tcoin.com>
2023-06-22 18:28:18 +0200

    fix

--- lib/screens/transactions/qr_code_sign/qr_code_sign.dart
+++ lib/screens/transactions/qr_code_sign/qr_code_sign.dart
@@ -9,6 +9,7 @@ import 'package:dart_wallet_v2/config/styles.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:io_takamaka_core_wallet/io_takamaka_core_wallet.dart';
+import 'package:qr_code_dart_scan/qr_code_dart_scan.dart';
 
 class QrCodeSign extends StatefulWidget {
   const QrCodeSign({super.key});
@@ -21,66 +22,52 @@ class _QrCodeSignState extends State<QrCodeSign> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(child:
-    CupertinoButton(
-        color: Styles.takamakaColor,
-        onPressed: verifyTransactionAndCallback,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Icon(CupertinoIcons.plus),
-            const SizedBox(width: 10),
-            const Text('testScan'),
-          ],
-        )),
-      // QRCodeDartScanView(
-      //   scanInvertedQRCode: true, // enable scan invert qr code ( default = false)
-      //   typeScan: TypeScan.live, // if TypeScan.takePicture will try decode when click to take a picture (default TypeScan.live)
-      //   // takePictureButtonBuilder: (context,controller,isLoading){ // if typeScan == TypeScan.takePicture you can customize the button.
-      //   //    if(loading) return CircularProgressIndicator();
-      //   //    return ElevatedButton(
-      //   //       onPressed:controller.takePictureAndDecode,
-      //   //       child:Text('Take a picture'),
-      //   //    );
-      //   // }
-      //   // resolutionPreset: = QrCodeDartScanResolutionPreset.high,
-      //   // formats: [ // You can restrict specific formats.
-      //   //   BarcodeFormat.QR_CODE,
-      //   //   BarcodeFormat.AZTEC,
-      //   //   BarcodeFormat.DATA_MATRIX,
-      //   //   BarcodeFormat.PDF_417,
-      //   //   BarcodeFormat.CODE_39,
-      //   //   BarcodeFormat.CODE_93,
-      //   //   BarcodeFormat.CODE_128,
-      //   //  BarcodeFormat.EAN_8,
-      //   //   BarcodeFormat.EAN_13,
-      //   // ],
-      //   onCapture: (Result result) {
-      //     verifyTransactionAndCallback(result);
-      //     // do anything with result
-      //     // result.text
-      //     // result.rawBytes
-      //     // result.resultPoints
-      //     // result.format
-      //     // result.numBits
-      //     // result.resultMetadata
-      //     // result.time
-      //   },
-      // )
+       QRCodeDartScanView(
+         scanInvertedQRCode: true, // enable scan invert qr code ( default = false)
+         typeScan: TypeScan.live, // if TypeScan.takePicture will try decode when click to take a picture (default TypeScan.live)
+         // takePictureButtonBuilder: (context,controller,isLoading){ // if typeScan == TypeScan.takePicture you can customize the button.
+         //    if(loading) return CircularProgressIndicator();
+         //    return ElevatedButton(
+         //       onPressed:controller.takePictureAndDecode,
+         //       child:Text('Take a picture'),
+         //    );
+         // }
+         // resolutionPreset: = QrCodeDartScanResolutionPreset.high,
+         // formats: [ // You can restrict specific formats.
+         //   BarcodeFormat.QR_CODE,
+         //   BarcodeFormat.AZTEC,
+         //   BarcodeFormat.DATA_MATRIX,
+         //   BarcodeFormat.PDF_417,
+         //   BarcodeFormat.CODE_39,
+         //   BarcodeFormat.CODE_93,
+         //   BarcodeFormat.CODE_128,
+         //  BarcodeFormat.EAN_8,
+         //   BarcodeFormat.EAN_13,
+         // ],
+         onCapture: (Result result) {
+           verifyTransactionAndCallback(result);
+           // do anything with result
+           // result.text
+           // result.rawBytes
+           // result.resultPoints
+           // result.format
+           // result.numBits
+           // result.resultMetadata
+           // result.time
+         },
+       )
     );
   }
 
-  // Future<void> verifyTransactionAndCallback(Result scannedData) async {
-  Future<void> verifyTransactionAndCallback() async {
-
+  Future<void> verifyTransactionAndCallback(Result scannedData) async {
     SimpleKeyPair skp = await WalletUtils.getNewKeypairED25519(
         Globals.instance.generatedSeed);
 
     TransactionBean tb = TransactionBean();
-    tb.message ="{\"from\":\"Ns559nPpty-fEOzV1Xx5sm49ITFTlNDAezW2BETh75c.\",\"to\":\"\",\"message\":\"a956z24q0b36rda314p9077xz889b2d5\",\"notBefore\":1693435948249,\"redValue\":null,\"greenValue\":null,\"transactionType\":\"BLOB\",\"transactionHash\":\"9iM_fc0FfgwEvt2Ya6D8swT5UhyYb1Cv_Y8CK8ksq9A.\",\"epoch\":null,\"slot\":null}";
-    tb.publicKey = "JzBcX2bJqZA82gOisilXwPd6szu1pnJMYZ7mamF1OgE.";
-    tb.randomSeed ="dqdP";
-    tb.signature = "NidUKZMZVIS3-HZTbPxIUzaxdkujf8b5q2LJP-PkP7ulRgl7JC-4ca1RUl7Pf8vkEyNHbWa9feK_BcDWoX3xBA..";
+    tb.message ="{\"from\":\"Ns559nPpty-fEOzV1Xx5sm49ITFTlNDAezW2BETh75c.\",\"to\":\"\",\"message\":\"7d383r_9_b6p42ycz4qq5610b3xq_7yp\",\"notBefore\":1693441529415,\"redValue\":null,\"greenValue\":null,\"transactionType\":\"BLOB\",\"transactionHash\":\"TzR1k8-ksBG0yvZ_3GsypXo1PtEniASi-82YyMXZnkc.\",\"epoch\":null,\"slot\":null}";
+    tb.publicKey = "Ns559nPpty-fEOzV1Xx5sm49ITFTlNDAezW2BETh75c.";
+    tb.randomSeed ="xLmY";
+    tb.signature = "yhrYv-iN6tj6Kt69duvr4flWQOsU40pEqw0nywsWo4bsW3DT66YllAUGynTCStxD6l9s6fuP31geXz2Piz4TAw..";
     tb.walletCypher = "Ed25519BC";
 
     // SimplePublicKey pubk = await skp.extractPublicKey();
